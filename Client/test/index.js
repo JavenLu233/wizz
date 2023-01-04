b49dc8d3406803759ec847ffbb8134c16460a6e9
JavenLu233 <1371994820@qq.com>
2023-01-04 14:08:25 +0800

    移动所有图片到/images

--- Client/test/index.js
+++
@@ -1,6 +0,0 @@
-const f = {}
-f.name = "javenlu"
-f["gg"] = "gg"
-console.log(f)
-
-{ "detail": [{ "loc": ["body", "msg_from"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "name"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "grade"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "major"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "email"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "phone"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "birthday"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "target_position"], "msg": "field required", "type": "value_error.missing" }, { "loc": ["body", "description"], "msg": "field required", "type": "value_error.missing" }] }
