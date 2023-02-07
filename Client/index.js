4203fcadc4ae4bb6b55ab6d97c791d5d121fd4e8
JavenLu233 <1371994820@qq.com>
2023-02-07 21:17:57 +0800

    修改 mobile_home.css

--- Client/index.js
+++ Client/index.js
@@ -8,7 +8,7 @@ bkg.style.width = sWidth + "px"
 window.onload = () => {
     if (/(Android|iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
         console.log("移动端")
-        document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="./mobile.css">`)
+        document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="./mobile_home.css">`)
     }
 
 }
